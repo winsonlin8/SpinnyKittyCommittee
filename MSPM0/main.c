b968d654ba0289485620ad65482e0714e30f14a0
karatekid0000 <91167735+karatekid0000@users.noreply.github.com>
2026-04-10 18:49:26 -0500

    Update main.c
    PWM code
    changes between fast and slow rotation every 2s

--- MSPM0/main.c
+++ MSPM0/main.c
@@ -1,10 +1,11 @@
+
 #include <ti/devices/msp/msp.h>
 #include <stdint.h>
 
 #define POWER_STARTUP_DELAY (16)
 #define PA25_MASK           (0x2000000)   // 1 << 25
 #define PA8_MASK            (0x00000100)  // 1 << 8
-#define PA12_MASK           (1 << 12)
+#define PA22_MASK           (1 << 22)
 
 #define SYSTEM_CLK (32000)
 #define FREQUENCY  (1000)
@@ -24,8 +25,7 @@ int main(void) {
     InitializeTimer_PWM();
 
     int pwm_counter = 0;
-    int motor_speed = 60; // 0-100% 
-    direction_t dir = FORWARD;
+    int motor_speed = 80; // 0-100% 
     uint32_t loop_counter = 0;
 
     while (1) {
@@ -34,45 +34,25 @@ int main(void) {
         // update counter (0-99)
         pwm_counter = (pwm_counter + 1) % 100;
 
-        // update direction
-        if (dir == FORWARD) {
-            GPIOA->DOUTSET31_0 = PA25_MASK;
-            GPIOA->DOUTCLR31_0 = PA8_MASK;
-        } else if (dir == REVERSE) {
-            GPIOA->DOUTCLR31_0 = PA25_MASK;
-            GPIOA->DOUTSET31_0 = PA8_MASK;
+        // direction
+        GPIOA->DOUTSET31_0 = PA25_MASK;
+        GPIOA->DOUTCLR31_0 = PA8_MASK;
+
+        if (is_pwm_on(pwm_counter, motor_speed)) {
+            GPIOA->DOUTSET31_0 = PA22_MASK; // ON (Active High)
         } else {
-            GPIOA->DOUTCLR31_0 = (PA25_MASK | PA8_MASK);
+            GPIOA->DOUTCLR31_0 = PA22_MASK; // OFF
         }
 
-        // changes speed every 2s
+        // // changes speed every 2s
         loop_counter++;
         if (loop_counter >= 2000) {
             loop_counter = 0; // reset
-            if (motor_speed == 60) {
-                motor_speed = 80; // 80%
-            } else if (motor_speed == 80) {
-                motor_speed = 100; // 100%
-            } else {
-                motor_speed = 60; // 60%
-            }
-        }
-
-        // update speed
-        if (is_pwm_on(pwm_counter, motor_speed)) {
-            GPIOA->DOUTSET31_0 = PA12_MASK; // ON (Active High)
-        } else {
-            GPIOA->DOUTCLR31_0 = PA12_MASK; // OFF
+            motor_speed = (motor_speed == 80) ? 30 : 80;
         }
     }
 }
 
-
-int is_pwm_on(int pwm_counter, int duty_percent) {
-    // pwm_counter (0-99)
-    // returns 1 if pwm_counter < duty_percent, and return 0 else
-    return (pwm_counter < duty_percent);
-}
 
 void InitializeGPIO_Motor(void) {
     GPIOA->GPRCM.RSTCTL = (GPIO_RSTCTL_KEY_UNLOCK_W | 
@@ -85,12 +65,17 @@ void InitializeGPIO_Motor(void) {
     delay_cycles(POWER_STARTUP_DELAY);
 
 
-    IOMUX->SECCFG.PINCM[55] = (IOMUX_PINCM_PC_CONNECTED | 0x00000001);
-    IOMUX->SECCFG.PINCM[19] = (IOMUX_PINCM_PC_CONNECTED | 0x00000001);
-    IOMUX->SECCFG.PINCM[34] = (IOMUX_PINCM_PC_CONNECTED | 0x00000001);
+    IOMUX->SECCFG.PINCM[(IOMUX_PINCM55)] =
+        (IOMUX_PINCM_PC_CONNECTED | ((uint32_t)0x00000001));
+    IOMUX->SECCFG.PINCM[(IOMUX_PINCM19)] =
+        (IOMUX_PINCM_PC_CONNECTED | ((uint32_t)0x00000001));
+    // IOMUX->SECCFG.PINCM[(IOMUX_PINCM34)] =
+    //     (IOMUX_PINCM_PC_CONNECTED | ((uint32_t)0x00000001));
+    IOMUX->SECCFG.PINCM[(IOMUX_PINCM47)] =
+        (IOMUX_PINCM_PC_CONNECTED | ((uint32_t)0x00000001));
 
-    GPIOA->DOESET31_0 = (PA25_MASK | PA8_MASK | PA12_MASK);
-    GPIOA->DOUTCLR31_0 = (PA25_MASK | PA8_MASK | PA12_MASK);
+    GPIOA->DOESET31_0 = (PA25_MASK | PA8_MASK | PA22_MASK);
+    GPIOA->DOUTCLR31_0 = (PA25_MASK | PA8_MASK | PA22_MASK);
 }
 
 void InitializeTimer_PWM(void) {
@@ -108,6 +93,10 @@ void InitializeTimer_PWM(void) {
     TIMG0->CPU_INT.IMASK |= GPTIMER_CPU_INT_IMASK_Z_SET;
     TIMG0->COMMONREGS.CCLKCTL = GPTIMER_CCLKCTL_CLKEN_ENABLED;
 
+    SYSCTL->SOCLOCK.PMODECFG = SYSCTL_PMODECFG_DSLEEP_STANDBY;
+    SCB->SCR |= SCB_SCR_SLEEPDEEP_Msk;
+    SYSCTL->SOCLOCK.MCLKCFG |= SYSCTL_MCLKCFG_STOPCLKSTBY_ENABLE;
+
     TIMG0->COUNTERREGS.LOAD = (SYSTEM_CLK / FREQUENCY); 
     TIMG0->COUNTERREGS.CTRCTL |= (GPTIMER_CTRCTL_EN_ENABLED);
     NVIC_EnableIRQ(TIMG0_INT_IRQn);
@@ -122,9 +111,14 @@ void TIMG0_IRQHandler(void) {
     }
 }
 
-static void delay_cycles(volatile uint32_t cycles)
-{
+static void delay_cycles(volatile uint32_t cycles) {
     while (cycles--) {
         __asm(" nop");
     }
+}
+
+int is_pwm_on(int pwm_counter, int duty_percent) {
+    // pwm_counter (0-99)
+    // returns 1 if pwm_counter < duty_percent, and return 0 else
+    return (pwm_counter < duty_percent);
 }
